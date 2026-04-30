d83edcc74959e26e421e490ed0e9d2b6da435ea8
PrashantK131 <rahulprashant2001@gmail.com>
2026-04-30 19:40:18 +0530

    Added css properties for the App and some additonal comments

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -4,6 +4,7 @@ function ToDoList({ todos, onDelete, onToggle, onEdit }){
 
     return(
         <div className="todo-list">
+            {/* Conditional Rendering: If the list is empty, show a message. Otherwise, map the items.*/}
             {todos.length === 0 ? (<p className="empty-msg">No tasks yet. Add one above!</p>
             ) : (todos.map((todo) => (
             <ToDoItem key={todo.id} todo={todo} onDelete={onDelete} onToggle={onToggle} onEdit={onEdit}/>))
